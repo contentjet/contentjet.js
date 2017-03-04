1821fcb2174cb5a9497a1e4f43dda5a9db85ca11
Will Dady <willdady@gmail.com>
2017-03-05 10:05:08 +1100

    Rewrite. Now using OAuth2 Resource Owner Password Credentials grant flow. Updated axios.

--- src/index.js
+++ src/index.js
@@ -1,58 +1,113 @@
 const axios = require('axios');
 
 
-const getClient = (options) => {
-  const instance = axios.create({
-    baseURL: `https://api.contentjet.io/v1/project/${options.projectUUID}/`,
+const authenticate = (username, password) => {
+  let data = {
+    username: username,
+    password: password,
+    grant_type: 'password'
+  };
+  return axios.post('https://api.contentjet.io/v1/authenticate/', data);
+}
+
+
+const tokenRefresh = (refreshToken) => {
+  let data = {
+    refresh_token: refreshToken,
+    grant_type: 'refresh_token'
+  };
+  return axios.post('https://api.contentjet.io/v1/token-refresh/', data);
+}
+
+
+const createInstance = (projectUUID, accessToken) => {
+  return axios.create({
+    baseURL: `https://api.contentjet.io/v1/project/${projectUUID}/`,
     timeout: 10000,
-    headers: { 'Authorization': `Bearer ${options.apiKey}` }
+    headers: { 'Authorization': `Bearer ${accessToken}` }
   });
+}
 
-  const getProject = () => {
-    return instance.get().then(result => result.data);
-  }
 
-  const getEntry = (entryUUID) => {
-    return instance.get(`entry/${entryUUID}`).then(result => result.data);
-  }
+const getClient = (options) => {
 
-  const listEntries = (queryParameters) => {
-    return instance.get('entry/', { params: queryParameters }).then(result => result.data);
-  }
+  let instance;
+  let timeoutId;
+  let projectUUID = options.projectUUID;
 
-  const getEntryType = (entryTypeUUID) => {
-    return instance.get(`entry-type/${entryTypeUUID}`).then(result => result.data);
-  }
+  return authenticate(options.username, options.password).then(response => {
 
-  const listEntryTypes = (queryParameters) => {
-    return instance.get('entry-type/', { params: queryParameters }).then(result => result.data);
-  }
+    let accessToken = response.data.access_token;
+    let refreshToken = response.data.refresh_token;
+    let expiresIn = response.data.expires_in;  // Seconds
 
-  const getMedia = (mediaUUID) => {
-    return instance.get(`media/${mediaUUID}`).then(result => result.data);
-  }
+    instance = createInstance(projectUUID, accessToken);
 
-  const listMedia = (queryParameters) => {
-    return instance.get('media/', { params: queryParameters }).then(result => result.data);
-  }
+    const refreshTimer = () => {
+      timeoutId = setTimeout(() => {
 
-  return {
-    project: {
-      get: getProject
-    },
-    entry: {
-      get: getEntry,
-      list: listEntries
-    },
-    entryType: {
-      get: getEntryType,
-      list: listEntryTypes
-    },
-    media: {
-      get: getMedia,
-      list: listMedia
+        tokenRefresh(refreshToken).then(response => {
+          accessToken = response.data.access_token;
+          refreshToken = response.data.refresh_token;
+          expiresIn = response.data.expires_in;
+          instance = createInstance(projectUUID, accessToken);
+          refreshTimer();
+        });
+
+      }, (expiresIn - 60) * 1000);
     }
-  }
+    refreshTimer();
+
+    const getProject = () => {
+      return instance.get().then(result => result.data);
+    }
+
+    const getEntry = (entryUUID) => {
+      return instance.get(`entry/${entryUUID}`).then(result => result.data);
+    }
+
+    const listEntries = (queryParameters) => {
+      return instance.get('entry/', { params: queryParameters }).then(result => result.data);
+    }
+
+    const getEntryType = (entryTypeUUID) => {
+      return instance.get(`entry-type/${entryTypeUUID}`).then(result => result.data);
+    }
+
+    const listEntryTypes = (queryParameters) => {
+      return instance.get('entry-type/', { params: queryParameters }).then(result => result.data);
+    }
+
+    const getMedia = (mediaUUID) => {
+      return instance.get(`media/${mediaUUID}`).then(result => result.data);
+    }
+
+    const listMedia = (queryParameters) => {
+      return instance.get('media/', { params: queryParameters }).then(result => result.data);
+    }
+
+    const destroy = () => {
+      clearTimeout(timeoutId);
+    }
+
+    return {
+      project: {
+        get: getProject
+      },
+      entry: {
+        get: getEntry,
+        list: listEntries
+      },
+      entryType: {
+        get: getEntryType,
+        list: listEntryTypes
+      },
+      media: {
+        get: getMedia,
+        list: listMedia
+      }
+    }
+  });
 
 };
 
